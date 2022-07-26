b26746e9e7f0c4f630c1c4691293304d4c502797
Piglet <piglet20181112@outlook.com>
2022-07-25 17:14:39 -0700

    test delete function in list page

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,8 +1,11 @@
-import { render, fireEvent, cleanup } from '@testing-library/react'
+import { render, fireEvent, cleanup, act, waitFor } from '@testing-library/react'
 import Home from '../pages/index'
+import UserContext  from '../contexts/UserContext';
 import '@testing-library/jest-dom'
+import mockAxios from 'jest-mock-axios'
 
 jest.mock('next/router', () => require('next-router-mock'));
+jest.mock('axios', () => require('jest-mock-axios'))
 
 describe('Home', () => {
   const renderApp = () => render(<Home/>);
@@ -15,12 +18,18 @@ describe('Home', () => {
     
   })
 
-  test('renders a heading', () => {
-    let {getByTestId} = renderApp();
+  test('renders initial UI with a user', () => {
+    let {getByTestId, getByText} = render(
+      <UserContext.Provider value={{list: [{id: 1, username: 'test', email: 'test@gmail.com', address: 'Jest street'}], push: jest.fn()}}>
+        <Home />
+      </UserContext.Provider>
+    );
     const heading = getByTestId('heading')
     const menus = getByTestId('menu')
     expect(heading).toBeInTheDocument()
     expect(menus.children[0]).toHaveTextContent('Create')
+    const user = getByText('test');
+    expect(user).toBeInTheDocument();
   })
 
   test('[edit button] selected row validation', () => {
@@ -37,6 +46,26 @@ describe('Home', () => {
     fireEvent.click(menus.children[2]);
     const warningAlert = getByTestId('warning-alert');
     expect(warningAlert).toBeVisible();
+  })
+
+  test('delete button test', () => {
+    let users = [{id: 1, username: 'test', email: 'test@gmail.com', address: 'Jest street'}];
+    let {getByTestId, getByText} = render(
+      <UserContext.Provider value={{list: users, push: (newUsers: any)=>{users=[...newUsers]}}}>
+        <Home />
+      </UserContext.Provider>
+    );
+    const menus = getByTestId('menu')
+    const user = getByText('test');
+    fireEvent.click(user);
+    fireEvent.click(menus.children[2]);
+    act(() => {
+      mockAxios.mockResponse({data:[]})
+    })
+    waitFor(()=>{
+      const noRows = getByText('No rows');
+      expect(noRows).toBeInTheDocument();
+    })
   })
   
 })
